7be77aaf5e8e9f7398a83371251c5352dcd10e49
DulceBucio <dulcenahomibucio@gmail.com>
2022-10-19 16:06:19 -0500

    funcioncriticos

--- situacionproblema.py
+++ situacionproblema.py
@@ -22,11 +22,19 @@ def anim(xcoord, ycoord):
         draw.rect(screen, (255,0,0), (xcoord[puntos], 300-ycoord[puntos], 10,10), 3)
         puntos += 1
 
+def criticos(ang,vinicial):
+    ang1 = math.radians(ang)
+    alcance = ((vinicial*vinicial)*math.sin(2*ang1))/9.81
+    alturamax = (2*vinicial*math.sin(ang1))/(2*9.81)
+
+    return alcance, alturamax
+
 
 init()
 screen = display.set_mode((800,600))
 animar = False
 clock = time.Clock()
+calibri = font.SysFont('Calibri', 10)
 
 while True:
     screen.fill((255,255,255))
@@ -36,6 +44,8 @@ while True:
         if e.type == KEYDOWN and e.key == K_1:
             animar = True
     
+    leyenda1 = calibri.render("Trayectoria actual: ", True, (0,0,0))
+    screen.blit(leyenda1, (0,0))
     if animar: 
         x1, y2 = coord(30,100)
         anim(x1, y2)
